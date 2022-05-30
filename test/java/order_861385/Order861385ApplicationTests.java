53a4d72ca51ec0e34b98a2e3ea6f03c1c42095d6
foxbatzsy <106297425+foxbatzsy@users.noreply.github.com>
2022-05-30 19:49:54 +0800

    Add files via upload

---
+++ test/java/order_861385/Order861385ApplicationTests.java
@@ -0,0 +1,13 @@
+package order_861385;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Order861385ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
